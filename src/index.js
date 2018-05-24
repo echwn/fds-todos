86b06fecc5bc44dc8f4ece0b0d6bbcbc7397ab0d
Lee Chaewon <lchaewn@gmail.com>
2018-05-24 15:54:36 +0900

    목록에 할 일 등록 기능

--- src/index.js
+++ src/index.js
@@ -8,13 +8,34 @@ const rootEl = document.querySelector('.root');
 
 const templates = {
   todoList: document.querySelector('#todo-list').content,
+  todoItem: document.querySelector('#todo-item').content,
+}
+
+function render(fragment) {
+  rootEl.textContent = '';
+  rootEl.appendChild(fragment);
 }
 
 async function indexPage() {
   const res = await todoAPI.get('/todos');
   const listFragment = document.importNode(templates.todoList, true);
-    
-  rootEl.appendChild(listFragment);
+
+  listFragment.querySelector('.todo-list__form').addEventListener('submit', async e => {
+    e.preventDefault();
+    const payload = {
+      body: e.target.elements.body.value
+    }
+    const res = await todoAPI.post(`/todos`, payload)
+    indexPage();
+  })
+
+  res.data.forEach(todo => {
+    const itemFragment = document.importNode(templates.todoItem, true);
+    itemFragment.querySelector('.todo-item__body').textContent = todo.body;
+    listFragment.appendChild(itemFragment);
+  })
+
+  render(listFragment);
 }
 
 indexPage();
