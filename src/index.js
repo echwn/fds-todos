c729d64bc4892c04f68541a6677795df5a5bcab5
Lee Chaewon <lchaewn@gmail.com>
2018-05-24 15:08:28 +0900

    템플릿 추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './index.scss';
+import axios from 'axios';
 
+const todoAPI = axios.create({
+  baseURL: process.env.API_URL
+});
 
+const rootEl = document.querySelector('.root');
+
+const templates = {
+  todoList: document.querySelector('#todo-list').content,
+}
+
+async function indexPage() {
+  const res = await todoAPI.get('/todos');
+  const listFragment = document.importNode(templates.todoList, true);
+    
+  rootEl.appendChild(listFragment);
+}
+
+indexPage();
